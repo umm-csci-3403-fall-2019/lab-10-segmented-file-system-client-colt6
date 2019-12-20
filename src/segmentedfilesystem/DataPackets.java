9de9a5401aa83379cc713a80913c7c8c268be493
ColtDahl <dahlcolt@gmail.com>
2019-12-20 08:52:22 -0600

    Do what I can with the segmented file system, won't be able to finish :(

--- src/segmentedfilesystem/DataPackets.java
+++ src/segmentedfilesystem/DataPackets.java
@@ -1,5 +1,34 @@
 package segmentedfilesystem;
+import java.io.File;
+import java.util.ArrayList;
 
 public class DataPackets {
 
+    int packetNumber;
+    byte[] packet;
+    int dataLength;
+    String filename;
+    boolean isfooter;
+
+    public void DataPacket (byte statusByte, byte fileID,int packetNumber, byte[] packetBuffer){
+        this.filename = filename;
+        this.packetNumber = packetNumber;
+        this.packet = packet;
+        this.dataLength = packetBuffer.length;
+        this.isfooter = statusByte % 4 == 3;
+    }
+
+    public ArrayList<Byte> getBoxedData(){
+        ArrayList<Byte> outputBytes = new ArrayList<>();
+        for (int i = 0; i < dataLength; i++){
+            outputBytes.add( packet[i]);
+        }
+        return outputBytes;
+    }
+
+
+    @Override
+    public void addToFile(File file) {
+        file.add(this);
+    }
 }
--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,16 +1,20 @@
 package segmentedfilesystem;
-
+import java.util.Collections;
 import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
-    
+
+     static DataPackets packets0, packets1, packets2 = new DataPackets();
+     static ArrayList<Byte> inputs = new ArrayList<>(3);
+
     public static void main(String[] args) throws IOException {
         byte[] sendBuf = new byte[0];
         int port = 6014;
-
 
         try {
             InetAddress inetaddress = InetAddress.getByName("csci-4409.morris.umn.edu");
@@ -21,7 +25,7 @@ public class Main {
 
             while(true){
                 byte[] newBuf = new byte[1028];
-                packet = new DatagramPacket(newBuf,)
+               // packet = new DatagramPacket(newBuf,)
 
             }
 
@@ -34,6 +38,39 @@ public class Main {
         }
 
 
+    }
+
+
+    private static void packetmatching(byte[] buf) {
+        if(inputs.contains(buf[1])) {
+            if(inputs.get(0) == buf[1]) {
+                if(buf[0] % 2 == 0) {
+                    byte[] name = Arrays.copyOfRange(buf, 2, buf.length);
+                    packets0.filename = new String(name);
+                } else {
+                    packets0.add(new DataPackets(buf));
+                }
+            }
+            if(inputs.get(1) == buf[1]) {
+                if(buf[0] % 2 == 0) {
+                    byte[] name = Arrays.copyOfRange(buf, 2, buf.length);
+                    packets1.filename = new String(name);
+                } else {
+                    packets1.add(new DataPackets(buf));
+                }
+            }
+            if(inputs.get(2) == buf[1]) {
+                if(buf[0] % 2 == 0) {
+                    byte[] name = Arrays.copyOfRange(buf, 2, buf.length);
+                    packets2.filename = new String(name);
+                } else {
+                    packets2.add(new DataPackets(buf));
+                }
+            }
+        } else {
+            inputs.add(buf[1]);
+            packetmatching(buf);
+        }
     }
 
 }
